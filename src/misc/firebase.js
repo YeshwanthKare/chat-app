89ee9013e1fe94e77958a3bfe6f50b33a9c203ad
YeshwanthKare <yesh.kare@gmail.com>
2021-08-20 16:36:39 +0530

    ADD: initialized firebase project

---
+++ src/misc/firebase.js
@@ -0,0 +1,12 @@
+import firebase from 'firebase/app';
+
+const firebaseConfig = {
+  apiKey: 'AIzaSyAZJbmDEIgEkFto2joQHxqU3W_2qD6A2WE',
+  authDomain: 'chat-web-app-b8ae4.firebaseapp.com',
+  projectId: 'chat-web-app-b8ae4',
+  storageBucket: 'chat-web-app-b8ae4.appspot.com',
+  messagingSenderId: '225521533724',
+  appId: '1:225521533724:web:41d3c279c2931eb9c370d9',
+};
+
+const app = firebase.initializeApp(firebaseConfig);
